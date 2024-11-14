584a0296c67eafbc5b56436078d70e2175b11314
klsova <kane.sova@gmail.com>
2024-11-14 10:29:20 +0200

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-<React.StrictMode>
+<StrictMode>
     <App />
-  </React.StrictMode>,
-  root
+  </StrictMode>,
 );
