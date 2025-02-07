4a9a8178bf036b6cd2b1f3d695b9b98a7ffa3347
klsova <kane.sova@gmail.com>
2025-02-07 13:02:59 +0200

    about me section added

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,24 @@ function App() {
     setDropdownOpen(!dropdownOpen);
   }
 
+  const scrollToAbout = () => {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+  }
+
   return (
     <>
       <header className='header'>
-        <div className="dropdownmenu">
-          <button className="ddbutton" onClick={toggleDropdown}>My portfolio</button>
+        <div className ='KS'>
+          <p>KS.</p>
+        </div>
+        <div className='button-container'>
+          <button className='aboutme-button' onClick={scrollToAbout}>About me</button>
+          <button className='ddbutton' onClick={toggleDropdown}>My projects</button>
           {dropdownOpen && (
-          <div className="dropdown-content">
+            <div className="dropdown-content">
             <a href="https://github.com/klsova/civswipe">CivSwipe</a>
             <a href="https://github.com/klsova/plasettaja">Plasettaja</a>
-            <a href="https://github.com/klsova/website">This site</a>
+            <a href="https://github.com/klsova/pythonpeli">PythonPeli</a>
         </div>
         )}
         </div>
@@ -33,6 +41,11 @@ function App() {
             <div className='second'><div>Student</div></div>
             <div className='third'><div>Techie</div></div>
           </section>
+
+          <div id='about' className='about'>
+            <h1>About me</h1>
+            <p>I am a third-year Information Technology student, an active student organization member, and the President of Digit ry. I have experience in such skills as team coordination, event planning, budgetary responsibility and broad communication skills. I have skills in Python, Java, JavaScript with a base knowledge in React and Express based applications. I also have skills in Photoshop and other artistic skills as well basic understanding of economics and business through my minor studies. My guild teammates see me as an happy and active team player who succeeds under guidance, but equally capable of taking the initiative as I become familiar with tasks and responsibilities.</p>
+          </div>
           <div className="logo-container">
             <p>My socials:</p>
             <div className='logos'>
