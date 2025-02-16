15c4b1936b89ccef123ccc1bf04cb63d676c232d
klsova <kane.sova@gmail.com>
2025-02-16 02:43:22 +0200

    -

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './components/Header/Header';
 import Main from './components/main/Main';
 import About from './components/about/About';
 import Projects from './components/projects/Projects';
-import TsContainer from './components/tsContainer/TsContainer';
+import TsContainer from './components/tscontainer/TsContainer';
 import './App.css';
 
 function App() {
