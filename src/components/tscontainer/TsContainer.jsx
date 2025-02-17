c4316f4a32dafcfb2f9b29183d7179e96eec48b4
klsova <kane.sova@gmail.com>
2025-02-17 20:24:17 +0200

    tech stack white icons

--- src/components/tscontainer/TsContainer.jsx
+++ src/components/tscontainer/TsContainer.jsx
@@ -4,33 +4,33 @@ import './TsContainer.css';
 function TsContainer() {
   return (
     <div className='ts-container'>
-          <h1>My tech stack</h1>
-          <div className='ts-grid'>
-            <div className='ts-item' data-tech='Python'>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" alt="Python" />
-            </div>
-            <div className='ts-item' data-tech='Java'>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" alt="Java" />
-            </div>
-            <div className='ts-item' data-tech='JavaScript'>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="JavaScript" />
-            </div>
-            <div className='ts-item' data-tech='React'>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="React" />
-            </div>
-            <div className='ts-item' data-tech='Express'>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt="Express" />
-            </div>
-            <div className='ts-item' data-tech='HTML'>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="HTML" />
-            </div>
-            <div className='ts-item' data-tech='CSS'>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="CSS" />
-            </div>
-            <div className='ts-item' data-tech='Photoshop'>
-              <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-plain.svg" alt="Photoshop" />
-            </div>
+      <h1>My tech stack</h1>
+      <div className='ts-grid'>
+        <div className='ts-item' data-tech='Python'>
+          <i className="devicon-python-plain"></i>
         </div>
+        <div className='ts-item' data-tech='Java'>
+          <i className="devicon-java-plain"></i>
+        </div>
+        <div className='ts-item' data-tech='JavaScript'>
+          <i className="devicon-javascript-plain"></i>
+        </div>
+        <div className='ts-item' data-tech='React'>
+          <i className="devicon-react-original"></i>
+        </div>
+        <div className='ts-item' data-tech='Express'>
+          <i className="devicon-express-original"></i>
+        </div>
+        <div className='ts-item' data-tech='HTML'>
+          <i className="devicon-html5-plain"></i>
+        </div>
+        <div className='ts-item' data-tech='CSS'>
+          <i className="devicon-css3-plain"></i>
+        </div>
+        <div className='ts-item' data-tech='Photoshop'>
+          <i className="devicon-photoshop-plain"></i>
+        </div>
+      </div>
     </div>
   );
 }
