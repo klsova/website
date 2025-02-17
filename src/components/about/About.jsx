48bc695a42e2525c00fe94c6eeffe154388228bd
klsova <kane.sova@gmail.com>
2025-02-17 17:27:44 +0200

    fixed about me css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,9 +5,11 @@ function About() {
   return (
     <div className='about-container'>
       <div id="about" className="about">
+        <h1>Who am I?</h1>
         <p>I am a third-year Information Technology student, an active student organization member, and the Chairperson of Digit ry. I have experience in such skills as team coordination, event planning, budgetary responsibility and broad communication skills.</p>
         <p>I have skills in Python, Java, JavaScript with a base knowledge in React and Express based applications. I also have skills in Photoshop and other artistic skills as well basic understanding of economics and business through my minor studies.</p>
         <p>My guildmates see me as an happy and active team player who succeeds under guidance, but equally capable of taking the initiative as I become familiar with tasks and responsibilities.</p>
+        <p>In my freetime I enjoy running, videogames, television shows and programming of course. I enjoy taking on new challenges and learning something new, which probably where my love and passion from coding stems from.</p>
       </div>
     </div>
   );
